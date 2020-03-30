e6c3a322379fbbe938a475bb12268c205cb98e6c
William Rogge <williamroggeiv@gmail.com>
2020-03-30 21:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   } if (string==="HELLO") {
     return "YES INDEED!"
+  } else if (string=== "I love you, Grandma."){
+    return "I love you, too."
   }
 
 }
