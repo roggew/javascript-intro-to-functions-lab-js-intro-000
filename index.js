053b4b484b25fa83dac14a5bdbb972aa3908ff32
William Rogge <williamroggeiv@gmail.com>
2020-03-30 21:24:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string==="hello"){
     return "I can't hear you!"
+  } if (string==="HELLO") {
+    return "YES INDEED!"
   }
 
 }
